7e2d6c743d46d2218a92dd0ac9c0d029e870c96d
Alberto Pingo <albertopingobusiness@gmail.com>
2024-11-06 05:25:59 +0000

    Add Wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.enable_wayland = false
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font 'JetBrains Mono NL'
+config.font_size = 12
+
+
+return config
