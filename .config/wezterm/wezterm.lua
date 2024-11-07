01b64f8763b6ab67dd6e7f6ef2edc1b09be8316f
Alberto Pingo <albertopingobusiness@gmail.com>
2024-11-07 23:25:57 +0000

    Update Wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,14 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+
 local config = {
 	enable_wayland = false,
 
 	max_fps = 144,
 	animation_fps = 144,
 
-	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font 'JetBrains Mono NL',
-	font_size = 12
+	color_scheme = "rose-pine",
+	font = wezterm.font("JetBrains Mono NL"),
+	font_size = 12,
 }
 
 return config
